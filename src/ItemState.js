4d5b0b2a6147ee19073d9ceee1edd69b33403428
Zach Lott <zacharylott94@gmail.com>
2020-04-16 22:39:52 -0500

    stripping out everything but the build

--- src/ItemState.js
+++
@@ -1,8 +0,0 @@
-//This factory function returns a simple object that helps organize data
-export function ItemState(desc = "Description", state = false, count = 0) {
-  return {
-    desc,
-    state,
-    count
-  };
-}
